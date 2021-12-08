e0bdaab5273e65b41dbdd4bda3ed496a203128c1
ianghel <angheliuliaioana@gmail.com>
2021-12-08 23:20:54 +0200

    Offer Modal + api

--- client/src/app/main-page/main-page.component.ts
+++ client/src/app/main-page/main-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendApiService } from '../services/backend-api.service';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import {NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-main-page',
@@ -8,14 +12,58 @@ import { BackendApiService } from '../services/backend-api.service';
 })
 export class MainPageComponent implements OnInit {
   public currentUserName!: string | null;
+  public currentUserId!: string | null;
   public curentUserType!: string | null;
+  public truck_id: string = "";
+  public trucks: any = [];
+  public status: string = "";
+  public leaving_date!: NgbDateStruct | null;
+  public arriving_date!: NgbDateStruct | null;
+  public leaving_place: string = "";
+  public arriving_place: string = "";
+  public price_km_empty: string = "";
+  public price_km_full: string = "";
+  public notes: string="";
 
-  constructor(public auth: BackendApiService) { }
+  constructor(public auth: BackendApiService, private parserFormatter: NgbDateParserFormatter) { }
 
   ngOnInit(): void {
     this.currentUserName = localStorage.getItem('user_fname');
     this.curentUserType = localStorage.getItem('user_type');
+    this.currentUserId = localStorage.getItem('user_id');
+
   }
 
+  getTrucks() {
+    // TODO GET TRUCKS - backend+frontend
+    this.trucks = [1, 2, 3, 4];
+  }
 
+  preparePostModal() {
+    this.truck_id = "";
+    this.status = "";
+    this.arriving_place = "";
+    this.leaving_place = "";
+    this.price_km_empty = "";
+    this.price_km_full = "";
+    this.notes="";
+    this.getTrucks();
+  }
+
+  isCarrier()
+  {
+    if( this.curentUserType =='carrier') return true;
+    else return false;
+  }
+
+  refresh() {
+    window.location.reload();
+  }
+
+  addOfferModal() {
+    let leav_date= this.parserFormatter.format(this.leaving_date);
+    let arr_date = this.parserFormatter.format(this.arriving_date);
+    this.auth.addOffer(this.currentUserId, this.truck_id, leav_date, this.leaving_place, arr_date, this.arriving_place, this.price_km_empty, this.price_km_full,this.notes).subscribe((res) => { });
+    this.refresh();
+  }
 }
--- client/src/app/services/backend-api.service.ts
+++ client/src/app/services/backend-api.service.ts
@@ -13,15 +13,21 @@ export class BackendApiService {
   login(email: string, password: string) {
     return this.http.post<any>(backendPath + 'login', { email: email, password: password });
   }
-  register(email: string, password: string, first_name: string, last_name: string , user_type:string) {
+
+  register(email: string, password: string, first_name: string, last_name: string, user_type: string) {
     return this.http.post<any>(backendPath + 'register', { email: email, password: password, first_name: first_name, last_name: last_name, user_type: user_type });
   }
+
   logout() {
-    this.http.post<any>(backendPath + 'logout', {}).subscribe(() => {});
+    this.http.post<any>(backendPath + 'logout', {}).subscribe(() => { });
     localStorage.removeItem('user_id');
     localStorage.removeItem('user_fname');
     localStorage.removeItem('user_type');
     this.router.navigateByUrl('login');
   }
+  addOffer(carrier_id: string | null, truck_id: string, leaving_date: string, leaving_place: string, arriving_date: string, arriving_place: string, price_km_empty: string, price_km_full: string, notes:string) {
+    return this.http.post<any>(backendPath + 'offer', { carrier_id: carrier_id, truck_id: truck_id, status: "Available", leaving_date: leaving_date, leaving_place: leaving_place, arriving_date: arriving_date, arriving_place: arriving_place, price_km_empty: price_km_empty, price_km_full: price_km_full, notes:notes})
+  }
+
 
 }
